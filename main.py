e536ffbe306273589933afaa72acf7ba4df2ebc5
RorusRom <rorusrusanov@gmail.com>
2023-10-20 20:03:58 +0300

    02

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def pnep(number):
+'''def pnep(number):
     if number % 2 == 0:
         return True
     else:
@@ -8,4 +8,42 @@ us = int(input("Введіть число "))
 
 res = pnep(us)
 
-print(res)
+print(res)'''
+
+'''def count_gol(t):
+    t = t.lower()
+
+    gol = "aeiouy"
+
+    golc = 0
+
+    for f in t:
+        if f in gol:
+            golc += 1
+
+    return golc
+
+us = input("Введіть рядок: ")
+res = count_gol(us)
+print(f"Кількість голосних у рядку: {res}")'''
+
+def factorial(n):
+    if n < 0:
+        return "Факториал невозможно вычислить"
+
+    elif n == 0 or n == 1:
+        return 1
+
+    else:
+        result = 1
+        for i in range(2, n+1):
+            result *= i
+        return result
+
+input = input("Введите целое положительное число: ")
+
+n = int(input)
+
+result = factorial(n)
+
+print(result)

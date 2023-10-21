ce01f6882ec8179bbd797363a187d94af0d11b41
RorusRom <rorusrusanov@gmail.com>
2023-10-21 12:10:50 +0300

    03

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
-'''def pnep(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
+import random
+class Student:
 
-us = int(input("Введіть число "))
+    def __init__(self, name=None):
+        self.name = name
+        self.alive = True
+        self.gladness = 50
+        self.progress = 0
 
-res = pnep(us)
+    def to_study(self):
+        print("Time to study!")
+        self.progress += 0.12
+        self.gladness -= 5
+    def to_chill(self):
+        print("Rest time")
+        self.gladness += 5
+        self.progress -= 0.1
+    def to_sleep(self):
+        print ("I will sleep!")
+        self.gladness +=3
 
-print(res)'''
+    def is_alive(self):
+        if self.progress < 0.5:
+            print('Cast out...')
+            self.alive + False
+        elif self.gladness <=0:
+            print("Depression")
+            self.alive = False
+        elif self.progress >5:
+            print('Passed externally...')
 
-'''def count_gol(t):
-    t = t.lower()
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {self.progress}")
 
-    gol = "aeiouy"
+    def live(self,day):
+        day = "Day" + str(day) + " of " + self.name + " life"
+        print(f"{day:=^50}")
+        live_cube = random.randint(1, 3)
+        if live_cube ==1:
+            self.to_study()
+        elif live_cube == 2:
+            self.to_sleep()
+        elif live_cube == 3:
+            self.to_chill()
+        self.end_of_day()
+        self.is_alive()
 
-    golc = 0
 
-    for f in t:
-        if f in gol:
-            golc += 1
-
-    return golc
-
-us = input("Введіть рядок: ")
-res = count_gol(us)
-print(f"Кількість голосних у рядку: {res}")'''
-
-def factorial(n):
-    if n < 0:
-        return "Факториал невозможно вычислить"
-
-    elif n == 0 or n == 1:
-        return 1
-
-    else:
-        result = 1
-        for i in range(2, n+1):
-            result *= i
-        return result
-
-input = input("Введите целое положительное число: ")
-
-n = int(input)
-
-result = factorial(n)
-
-print(result)
+nick = Student (name="Nick")
+for i in range(365):
+    if nick.alive == False:
+        break
+    nick.live(i)
